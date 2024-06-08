fd805858ef7bd68617e5df0fa1a30520c813db1e
Jaakko Mustalahti <jaska.mustalahti@gmail.com>
2024-06-08 14:51:17 +0300

    hide instead of conditional render for performance

--- src/app/components/multi-step-form/multi-step-form.tsx
+++ src/app/components/multi-step-form/multi-step-form.tsx
@@ -8,24 +8,28 @@ import { Step3 } from "./step3";
 export function MultiStepForm() {
   const [step, setStep] = useState(0);
 
-  const Steps = [Search, Step2, Step3];
+  const Steps: { component: () => JSX.Element; button: string }[] = [
+    { component: Search, button: "Search" },
+    { component: Step2, button: "Step 2" },
+    { component: Step3, button: "Step 3" },
+  ];
 
   return (
     <div className="max-w-md mx-auto mt-10 p-4 border rounded shadow">
       <h1 className="text-2xl font-bold text-center mb-6">Steps</h1>
       <div className="flex flex-col">
         <div className="flex justify-between mb-8">
-          {Steps.map((Component, index) => (
+          {Steps.map(({ component: Component, button }, index) => (
             <button key={`${Component.name}-button`} onClick={() => setStep(index)} type="button">
-              {Component.name}
+              {button}
             </button>
           ))}
         </div>
-        <div>
-          {Steps.map(
-            (Component, index) => step === index && <Component key={`${Component.name}-step`} />
-          )}
-        </div>
+        {Steps.map(({ component: Component }, index) => (
+          <div key={`${Component.name}-step`} className={step === index ? "block" : "hidden"}>
+            <Component />
+          </div>
+        ))}
       </div>
     </div>
   );
