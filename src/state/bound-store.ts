79000475caddca58522dd7884b424475b53fd289
Jaakko Mustalahti <jaska.mustalahti@gmail.com>
2024-10-12 13:30:40 +0300

    ensure dates are revived as date objects

--- src/state/bound-store.ts
+++ src/state/bound-store.ts
@@ -1,11 +1,36 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 import { createConfigSlice } from "./config/config";
 import { createSearchSlice } from "./search/search";
 import { ConfigSlice } from "./config/types";
 import { SearchSlice } from "./search/types";
 import { storeScopusApiKeys } from "./effects/scopus/scopus";
+
+function isIsoDateString(v: unknown): v is string {
+  if (typeof v !== "string") {
+    return false;
+  }
+
+  // Check if the string matches the ISO 8601 date format (YYYY-MM-DDTHH:mm:ss.sssZ)
+  const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{1,3})?Z$/;
+  if (!isoDateRegex.test(v)) {
+    return false;
+  }
+
+  // Check if the value can be successfully converted to a valid Date object
+  const date = new Date(v);
+  return !Number.isNaN(date.getTime());
+}
+
+const storage = createJSONStorage(() => localStorage, {
+  reviver: (key, value) => {
+    if (isIsoDateString(value)) {
+      return new Date(value);
+    }
+    return value;
+  },
+});
 
 export const useBoundStore = create<ConfigSlice & SearchSlice>()(
   persist(
@@ -15,6 +40,7 @@ export const useBoundStore = create<ConfigSlice & SearchSlice>()(
     }),
     {
       name: "bound-store",
+      storage,
       onRehydrateStorage: () => {
         return (state) => {
           if (state) {
